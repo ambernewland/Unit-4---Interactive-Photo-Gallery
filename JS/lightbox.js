1b24119db75cec1b26ef585e301813359989c793
ambernewland <ambernewland@Ambers-iMac.local>
2017-04-05 11:40:15 +0100

    lightbox installed - fixes needed

---
+++ JS/lightbox.js
@@ -0,0 +1,12 @@
+<script>
+lightbox.option({
+        'resizeDuration': 400,
+        'wrapAround': false,
+        'alwaysShowNavOnTouchDevices': true,
+        'max-width': 200,
+    })
+</script>
+
+
+
+
